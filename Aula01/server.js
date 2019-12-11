41e6a0fae466621fe1a6bc1e412baf116ca56b6a
Rafael Thomazelli <rafael.thomazelli@gmail.com>
2019-12-10 21:09:03 -0300

    inicio api server

--- Aula01/server.js
+++ Aula01/server.js
@@ -1,9 +1,9 @@
 const http = require('http')
 
-const requisicao = function(request, response){
-    response.writeHead(200, { 'Content-Type' : 'text/html'})
-    response.write('<h1>Texto a ser exibido no Browser</h1>')
-    response.end()
+const requisicao = function(_, y){
+    y.writeHead(200, { 'Content-Type' : 'text/html'})
+    y.write('<h1>Texto a ser exibido no Browser</h1>')
+    y.end()
 }
 
 const server = http.createServer(requisicao)
